2d66db95124503c7b4480d0c84ab857c052366fd
Tiago Santos Costa <tiagocostta@outlook.com>
2024-07-04 12:36:27 -0300

    mudança de servidor

--- SRC/controllers/userControllers.js
+++ SRC/controllers/userControllers.js
@@ -2,15 +2,15 @@ const connection = require('../models/connection');
 
 
 
- exports.saveUser = async (req, res) => {
+exports.saveUser = async (req, res) => {
     // Extraia os dados do usuário do corpo da requisição
     const { name, email, message } = req.body;
 
     try {
         // Insira os dados na tabela users
         await connection.query(
-            'INSERT INTO users (usuario, email, mensagem) VALUES (?, ?, ?)',
-            [name, email, message]  
+            'INSERT INTO users (name, email, message) VALUES (?, ?, ?)',
+            [name, email, message]
         );
 
         res.send('Usuário salvo com sucesso!');
@@ -22,7 +22,6 @@ const connection = require('../models/connection');
 
 
 exports.listUser = async (req, res) => {
-    console.log('testeee');
     try {
         // Busque os dados da tabela users
         const [rows, fields] = await connection.query(
--- SRC/server.js
+++ SRC/server.js
@@ -1,19 +1,14 @@
 const app = require('./app');
 require('dotenv').config();
 
+const PORT = process.env.PORT || 8000; // Pega a porta do arquivo .env ou usa 8000 como padrão
 
+const server = app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
 
+// Para lidar com qualquer erro de inicialização do servidor
+server.on('error', (error) => {
+    console.error('Erro ao iniciar o servidor:', error);
+});
 
-
- 
-
-
-app.listen( () => console.log(`Servidor rodando OK!`));
-
-
-
-// const PORT = 8000; // Escolha a porta desejada (por exemplo, 8000)
-
-// app.listen(PORT, () => {
-//   console.log(`Servidor rodando OK! Porta: ${PORT}`);
-// });
