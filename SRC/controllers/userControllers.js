f32f8680b242aa5ae9e08775c029bf7f60aca662
Tiago Santos Costa <tiagocostta@outlook.com>
2024-07-16 12:18:18 -0300

    add feat CRUD

--- SRC/controllers/userControllers.js
+++ SRC/controllers/userControllers.js
@@ -3,22 +3,37 @@ const connection = require('../models/connection');
 
 
 exports.saveUser = async (req, res) => {
-    // Extraia os dados do usuário do corpo da requisição
-    const { name, email, message } = req.body;
+    const { id, name, email, message } = req.body;
+    console.log(req.body);
 
     try {
-        // Insira os dados na tabela users
-        await connection.query(
-            'INSERT INTO users (name, email, message) VALUES (?, ?, ?)',
-            [name, email, message]
-        );
+        if (id) {
+            // Atualiza os dados do usuário existente
+            const [result] = await connection.query(
+                'UPDATE users SET name = ?, email = ?, message = ? WHERE id = ?',
+                [name, email, message, id]
+            );
 
-        res.send('Usuário salvo com sucesso!');
+            if (result.affectedRows === 0) {
+                return res.status(404).send('Usuário não encontrado.');
+            }
+
+            res.send('Usuário atualizado com sucesso!');
+        } else {
+            // Insere novos dados do usuário
+            await connection.query(
+                'INSERT INTO users (name, email, message) VALUES (?, ?, ?)',
+                [name, email, message]
+            );
+
+            res.send('Usuário salvo com sucesso!');
+        }
     } catch (error) {
         console.error(error);
         res.status(500).send('Ocorreu um erro ao salvar o usuário.');
     }
 };
+
 
 
 exports.listUser = async (req, res) => {
@@ -33,6 +48,26 @@ exports.listUser = async (req, res) => {
     } catch (error) {
         console.error(error);
         res.status(500).send('Ocorreu um erro ao buscar os usuários.');
+    }
+};
+
+
+exports.editUser = async (req, res) => {
+    const userId = req.params.id; // Acessa o parâmetro da URL
+
+    console.log('ID recebido:', userId);
+
+    try {
+        // Consulta SQL para buscar os dados do usuário com base no userId
+        const [users] = await connection.query('SELECT * FROM users WHERE id = ?', [userId]);
+        // Como você está esperando apenas um usuário com um id específico, pegue o primeiro elemento do array
+        const user = users[0];
+
+        // Envie os dados do usuário como resposta
+        res.json(user);
+    } catch (error) {
+        console.error('Erro ao buscar dados do usuário:', error);
+        res.status(500).send('Ocorreu um erro ao buscar os dados do usuário.');
     }
 };
 
--- SRC/router.js
+++ SRC/router.js
@@ -7,6 +7,8 @@ router.post('/user/save', userControllers.saveUser);
 
 router.get('/user/list', userControllers.listUser);
 
+router.get('/user/list/:id', userControllers.editUser);
+
 
 
 module.exports = router;
